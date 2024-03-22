3936d327bcec9814f597b1c94d89eac2891d847d
swsxm <129607562+swsxm@users.noreply.github.com>
2024-03-22 14:04:02 +0100

    removed debugging comment for deploy

--- speiseplan_projekt/src/lib/mongodb.js
+++ speiseplan_projekt/src/lib/mongodb.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 export const connectMongoDB = async () => {
     // Connect to MongoDB
     try {
-        console.log(process.env.MONGODB_URI)
         await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
         console.log("Connected to MongoDB");
     }
