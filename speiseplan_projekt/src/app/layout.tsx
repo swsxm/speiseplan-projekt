62f0ab453f11cfc3b4a83a555bf1180b11263b7f
swsxm <129607562+swsxm@users.noreply.github.com>
2024-03-24 18:40:31 +0100

    visual fix

--- speiseplan_projekt/src/app/layout.tsx
+++ speiseplan_projekt/src/app/layout.tsx
@@ -1,22 +1,32 @@
+// Import required modules and styles
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+// Define Inter font
 const inter = Inter({ subsets: ["latin"] });
 
+// Metadata for the website
 export const metadata: Metadata = {
   title: "Kantinerado",
   description: "Speiseplan-Projekt",
 };
 
+// RootLayout component
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="max-w-[1640px] mx-auto lg:px-48 px-4">{children}</body>
+    <html lang="en" className="h-full">
+      <body className="h-full">
+        {/* Wrap the children inside a container div */}
+        <div className="min-h-full max-w-[1640px] mx-auto lg:px-48 px-4 flex flex-col">
+          {/* Render the children */}
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
