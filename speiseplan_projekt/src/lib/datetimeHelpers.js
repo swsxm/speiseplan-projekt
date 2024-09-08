ac65bcc82e9263a600d603e355438bfee87d9785
swsxm <129607562+swsxm@users.noreply.github.com>
2024-09-08 20:08:25 +0200

    validating ordering process

--- speiseplan_projekt/src/lib/datetimeHelpers.js
+++ speiseplan_projekt/src/lib/datetimeHelpers.js
@@ -17,3 +17,13 @@ export function dayOfWeek(date){
  */
     return new Date(date).getDay();
 }
+
+export const dayNameToNumber = {
+    "Montag": 1,
+    "Dienstag": 2,
+    "Mittwoch": 3,
+    "Donnerstag": 4,
+    "Freitag": 5,
+    "Samstag": 6,
+    "Sonntag": 7
+  };
